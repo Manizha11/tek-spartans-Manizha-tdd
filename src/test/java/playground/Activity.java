7ecb6814143cb2ef2ae716ae7a5385ebc45d40be
Manizha11 <manizha11@gmail.com>
2024-08-12 21:39:34 -0400

    Activity

---
+++ src/test/java/playground/Activity.java
@@ -0,0 +1,36 @@
+package playground;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Activity {
+    /*
+    Write a test that navigate to Facebook.com
+    and print Title
+     */
+    private WebDriver driver
+    @BeforeMethod
+    public void FaceBookTest(){
+        driver = new EdgeDriver();
+        driver.get("facebook.com");{
+
+            @Test
+                    public void testFaceBookTitle(){
+                String applicationTitle = driver.getTitle();
+                Assert.assertEquals(applicationTitle," Facebook- log in or  sign up");
+
+            }
+            @AfterMethod
+                    public void cleanUpTest(){
+                driver.quit();
+            }
+
+        }
+
+    }
+}
